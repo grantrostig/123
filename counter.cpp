679c2872a188e5642932bc1ffb8c380aa24e6e1f
Grant Rostig <grant.github@emailmeinfo.com>
2024-05-24 16:17:44 -0500

    add missing files

---
+++ counter.cpp
@@ -0,0 +1,8 @@
+#include "counter.hpp"
+
+/* std::vector<std::string> words{ "Bee", "A",      "E",    ".", "Core",   "Ree", "Oh",      ".",     "Shan",    "Shee",   "Tay", ".",
+                                "Zee", ".",      "Zano", ".", "Happy",  ",",   "healthy", ",",     "wealthy", ",",      "and", "wise",
+                                ".",   "Loving", "you",  ",", "loving", "me",  "with",    "grace", "and",     "beauty", "." };
+*/
+
+// Counter::Counter() {}
