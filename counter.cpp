c1e6f43e4ab20a4a0011fad92ccd1e5db036d2b7
Grant Rostig <grant.github@emailmeinfo.com>
2025-01-08 13:54:20 -0600

    cleaned out, now ready for non public new examples, runs.

--- counter.cpp
+++ counter.cpp
@@ -1,8 +1,3 @@
 #include "counter.hpp"
 
-/* std::vector<std::string> words{ "Bee", "A",      "E",    ".", "Core",   "Ree", "Oh",      ".",     "Shan",    "Shee",   "Tay", ".",
-                                "Zee", ".",      "Zano", ".", "Happy",  ",",   "healthy", ",",     "wealthy", ",",      "and", "wise",
-                                ".",   "Loving", "you",  ",", "loving", "me",  "with",    "grace", "and",     "beauty", "." };
-*/
-
 // Counter::Counter() {}
